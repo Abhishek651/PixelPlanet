f183c99aaaf650e1252bf50666c58c355328d823
Abhishek <cyberlord700@gmail.com>
2025-10-04 17:49:56 +0530

    refactor: remove ChallengeContext

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { ChallengeProvider } from './context/ChallengeContext.jsx'
 import { SettingsProvider } from './context/SettingsContext.jsx'
 
 
@@ -15,9 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      {/* context provider that likely holds authentication state (user, login(), logout(), etc.) and makes it available to every component inside app */}
     <AuthProvider>
       <SettingsProvider>
-        <ChallengeProvider>
           <App />
-        </ChallengeProvider>
       </SettingsProvider>
     {/* Your actual application component (router, layout, pages…). It receives the context values via useContext(AuthContext) or the AuthProvider’s consumer API.	Keeps your UI code clean and focused on presentation/logic */}
     </AuthProvider>
